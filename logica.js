a9c5c714e7a6ba7767d86b8c0eebc20e62241a3e
ANDERS123321 <pachedomgab@gmail.com>
2025-06-26 14:45:56 -0500

    ultima version de web

--- logica.js
+++ logica.js
@@ -4,56 +4,56 @@ const mensaje = document.getElementById("mensaje");
 const filtroCarrera = document.getElementById("filtrocarrera");
 
 function postEstudiante(estudiante) {
-    let estudiantes = JSON.parse(localStorage.getItem("estudiantes")) || [];
-    estudiantes.push(estudiante);
-    localStorage.setItem("estudiantes", JSON.stringify(estudiantes));
+  let estudiantes = JSON.parse(localStorage.getItem("estudiantes")) || [];
+  estudiantes.push(estudiante);
+  localStorage.setItem("estudiantes", JSON.stringify(estudiantes));
 }
 
 function getEstudiantes(carrera = "todos") {
-    let estudiantes = JSON.parse(localStorage.getItem("estudiantes")) || [];
-    if (carrera !== "todos") {
-        estudiantes = estudiantes.filter(e => e.carrera === carrera);
-    }
-    return estudiantes;
+  let estudiantes = JSON.parse(localStorage.getItem("estudiantes")) || [];
+  if (carrera !== "todos") {
+    estudiantes = estudiantes.filter(e => e.carrera === carrera);
+  }
+  return estudiantes;
 }
 
 function mostrarEstudiantes(carrera = "todos") {
-    const lista = getEstudiantes(carrera);
-    tabla.innerHTML = "";
-    lista.sort((a, b) => a.nombre.localeCompare(b.nombre));
-    lista.forEach((e, i) => {
+  const lista = getEstudiantes(carrera);
+  tabla.innerHTML = "";
+  lista.sort((a, b) => a.nombre.localeCompare(b.nombre));
+  lista.forEach((e, i) => {
     const fila = `
-        <tr>
+      <tr>
         <td>${i + 1}</td>
         <td>${e.nombre}</td>
         <td>${e.edad}</td>
         <td>${e.carrera}</td>
-        </tr>
+      </tr>
     `;
     tabla.innerHTML += fila;
-        });
+  });
 }
 
 formulario.addEventListener("submit", e => {
-    e.preventDefault();
-    const nombre = document.getElementById("nombre").value.trim();
-    const edad = parseInt(document.getElementById("edad").value);
-    const carrera = document.getElementById("carrera").value;
+  e.preventDefault();
+  const nombre = document.getElementById("nombre").value.trim();
+  const edad = parseInt(document.getElementById("edad").value);
+  const carrera = document.getElementById("carrera").value;
 
-    if (edad <= 16) {
-    mensaje.textContent = "⚠️ La edad debe ser mayor a 16.";
+  if (edad <= 16) {
+    mensaje.textContent = "⚠ La edad debe ser mayor a 16.";
     return;
-    }
+  }
 
-    const estudiante = { nombre, edad, carrera };
-    postEstudiante(estudiante);
-    mensaje.textContent = "";
-    formulario.reset();
-    mostrarEstudiantes(filtroCarrera.value);
+  const estudiante = { nombre, edad, carrera };
+  postEstudiante(estudiante);
+  mensaje.textContent = "";
+  formulario.reset();
+  mostrarEstudiantes(filtroCarrera.value);
 });
 
 filtroCarrera.addEventListener("change", () => {
-    mostrarEstudiantes(filtroCarrera.value);
+  mostrarEstudiantes(filtroCarrera.value);
 });
 
 mostrarEstudiantes();
